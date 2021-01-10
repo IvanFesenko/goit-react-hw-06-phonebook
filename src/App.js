d882d041a60fbd434c63770dd7e02da86df43301
Ivan Fesenko <61904995+IvanFesenko@users.noreply.github.com>
2021-01-10 11:00:14 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className={styles.container}></div>;
+  }
+}
+
+export default App;
